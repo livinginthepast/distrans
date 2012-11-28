4c37128ddc8e35e77eaef9c8d21d080a70b69355
Eric Saxby <sax@livinginthepast.org>
2012-11-27 23:45:13 -0800

    start responder

--- server.c
+++ server.c
@@ -2,26 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/types.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#define READ_BUFFER 8026
+
+void respond(int socket) {
+  long req;
+  static char read_buffer[READ_BUFFER+1];
+
+  req = read(socket, read_buffer, READ_BUFFER);
+
+  if(req > 0 && req < READ_BUFFER) {
+    /* request is valid, terminate buffer */
+    read_buffer[req] = 0;
+  } else {
+    read_buffer[0] = 0;
+  }
+
+  sprintf("request: %s", buffer);
+
+  close(socketfd);
+  exit(1);
+}
+
 int main(int argc, char *argv[]) {
-  int i, listenfd, connfd;
-  struct sockaddr_in serv_addr;
+  int i, pid, verbose, listenfd, socketfd;
+  socklen_t length;
+  static struct sockaddr_in serv_addr;
+	static struct sockaddr_in cli_addr;
 
-  char *address = "0.0.0.0";
   int port = 80;
+  char *address = "0.0.0.0";
 
-  char sendBuff[1025];
-  time_t ticks;
 
   for (i = 1; i < argc; i++) {
     if (strcmp(argv[i], "-p") == 0) {
       port = atoi(argv[i+1]);
     } else if (strcmp(argv[i], "-l") == 0) {
       address = argv[i+1];
+    } else if (strcmp(argv[i], "-v") == 0) {
+      verbose = 1;
     }
   }
+
+  if (verbose)
+    (void) sprintf("listening on: %s:%d\n", address, port);
 
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
   memset(&serv_addr, '0', sizeof(serv_addr));
@@ -35,5 +63,18 @@ int main(int argc, char *argv[]) {
 
   listen(listenfd, 10);
 
-  printf("listen: %s:%d\n", address, port);
+  while(1) {
+    length = sizeof(cli_addr);
+    socketfd = accept(listenfd, (struct sockaddr*)&cli_addr, &length);
+
+    pid = fork();
+    if(pid == 0) {
+      (void)close(listenfd);
+      respond(socketfd);
+    } else if (pid > 0) {
+      (void)close(socketfd);
+    }
+  }
+
+  return 0;
 }
